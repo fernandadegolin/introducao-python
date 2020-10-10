0582bece60d54d87915f59ff3317d2b4c9762f32
Fernanda Degolin <57374044+fernandadegolin@users.noreply.github.com>
2020-10-10 11:47:15 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,5 @@
+valor = 25
+fornecedor = valor / 2
+brindes = int(input('Quantos brindes você vai fazer? '))
+
+print(f'O valor que você vai precisar dar de entrada será {brindes * fornecedor:.2f}')
