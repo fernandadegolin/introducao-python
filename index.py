aa2ae7372115c379741b71e49a9dbffdef44d092
Fernanda Degolin <57374044+fernandadegolin@users.noreply.github.com>
2020-10-10 12:59:13 -0300

    Update index.py

--- index.py
+++ index.py
@@ -35,3 +35,16 @@ print(nome2)
 qual seu nome completo? fe degolin
 fe
 degolin
+
+
+******* Exercicio 4 *******
+rosa, roxo, azul = map(int, input("Informe separado por virgulas quantas participantes por cor: ").split(","))
+total_participantes = rosa + roxo + azul
+
+porcentagem_rosa = rosa / total_participantes * 100
+porcentagem_roxo = roxo / total_participantes * 100
+porcentagem_azul = azul / total_participantes * 100
+
+print(f"{porcentagem_rosa:.2f} % de pessoas preferem rosa.")
+print(f"{porcentagem_roxo:.2f} % de pessoas preferem roxo.")
+print(f"{porcentagem_azul} % de pessoas preferem azul.")
